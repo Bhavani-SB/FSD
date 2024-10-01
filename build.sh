d3b0849ce3523ecd8187b260c07d4517daf3695e
Bhavani-SB <sb.bhavani.sb@gmail.com>
2024-10-01 21:03:08 +0530

    15th commit

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-if[[ $CREATE_SUPERUSER]];
+if[[ "$CREATE_SUPERUSER" == "True"]];
 then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
