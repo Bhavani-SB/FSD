c15008ed552abd84e226b34dfe918702123b83ad
Bhavani-SB <sb.bhavani.sb@gmail.com>
2024-10-01 21:11:28 +0530

    16th commit

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
+
+# Install the requirements
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
-if[[ "$CREATE_SUPERUSER" == "True"]];
-then
-  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+
+# Create a superuser if the CREATE_SUPERUSER environment variable is set to "True"
+if [[ "$CREATE_SUPERUSER" == "True" ]]; then
+    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
