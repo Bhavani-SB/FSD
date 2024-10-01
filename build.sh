7042bef0b532723fa5d0c126b198e9b2bf9821f0
Bhavani-SB <sb.bhavani.sb@gmail.com>
2024-10-01 21:37:40 +0530

    20th commit

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic 
+python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
