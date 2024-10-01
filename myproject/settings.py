7676166842a009d9911399589cab7ea5c4cfa41d
Bhavani-SB <sb.bhavani.sb@gmail.com>
2024-10-01 19:56:12 +0530

    13th commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,6 +94,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
